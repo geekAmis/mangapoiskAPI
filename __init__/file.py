b10ae2fab4473b6a2a9eb57800973b3c77a6214d
geekAmis <gik.lavrov.86@gmail.com>
2023-09-27 10:43:15 +0300

    lib's

---
+++ __init__/file.py
@@ -0,0 +1,10 @@
+import os,requests
+from bs4 import BeautifulSoup as bs
+from fake_headers import Headers as Hd
+import json,random
+
+
+from fastapi import FastAPI
+from typing import Annotated
+from fastapi import Query
+from fastapi.responses import HTMLResponse
